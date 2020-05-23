8c768474299726d08099111f207fc413606391ae
YuriiRakhimov <yyuuraganov@gmail.com>
2020-05-23 13:25:45 +0300

    add models , change serializers

--- backend/userauth/models.py
+++ backend/userauth/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    phone = models.CharField(max_length=12,primary_key=True)
+    password = models.CharField(max_length=20)
